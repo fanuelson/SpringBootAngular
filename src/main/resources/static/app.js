6b4680b6d130d9e4752e98b8da32c25350de6af7
fanuel nunes <fanuelson@gmail.com>
2016-09-27 01:30:28 -0300

    ui-router implementado.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,9 +1,32 @@
 var depends = [
+	'ui.router'
 ];
 
 var app = angular.module('demoApp', depends);
 
 app.constant("APP_CONFIG", {
-	"REST_BASE_URL" : "http://localhost:8080",
+	"REST_BASE_URL" : "http://localhost:8080/SpringAngularApp",
 	"DEFAULT_PAGE_SIZE": 10
 });
+
+var routeConfig = function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+	    .state("produtos", {
+	      url: "/",
+	      views: {
+	          "starterContent":{
+	              templateUrl: "produto/produto.html",
+	          }
+	      }
+	    });
+}
+
+var configRoute = [
+	'$stateProvider', 
+	'$urlRouterProvider',
+	routeConfig
+]
+
+
+app.config(configRoute);
