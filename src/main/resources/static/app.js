47a830c4793fb7d2d496a3b873249d67f32d4685
fanuel nunes <fanuelson@gmail.com>
2016-09-29 00:30:34 -0300

    Template + Menu.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,21 +4,43 @@ var depends = [
 
 var app = angular.module('demoApp', depends);
 
-app.constant("APP_CONFIG", {
-	"REST_BASE_URL" : "http://localhost:8080/SpringAngularApp",
-	"DEFAULT_PAGE_SIZE": 10
-});
+app.constant(
+	"APP_CONFIG", {
+		"REST_BASE_URL" : "http://localhost:8080/SpringAngularApp",
+		"DEFAULT_PAGE_SIZE": 10
+	}
+);
 
 var routeConfig = function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider
+	    .state("home", {
+		      url: "/",
+		      views: {
+		          "menuContent":{
+		              templateUrl: "menu.html",
+		          },
+		          "starterContent":{
+		              template: "<h1>HELLO<h1/>",
+		          },
+		          "footerContent":{
+		              templateUrl: "footer.html",
+		          },
+		      }     
+	    })
 	    .state("produtos", {
-	      url: "/",
+	      url: "/produtos",
 	      views: {
+	          "menuContent":{
+	              templateUrl: "menu.html",
+	          },
 	          "starterContent":{
 	              templateUrl: "produto/produto.html",
-	          }
-	      }
+	          },
+	          "footerContent":{
+	              templateUrl: "footer.html",
+	          },
+	      }     
 	    });
 }
 
