63dc09863737094ff4c73f08e53da0f867c7b786
fanuel nunes <fanuelson@gmail.com>
2016-10-11 20:14:29 -0300

    refatorações.

--- src/main/resources/static/login/LoginController.js
+++ src/main/resources/static/login/LoginController.js
@@ -3,7 +3,9 @@ function loginController($scope, APP_CONFIG) {
 	$scope.user = {};
 	
 	$scope.login = function() {
-		console.log(user.login + " " + user.senha);
+		console.log($scope.user.login + " " + $scope.user.senha);
+		
+		
 	}
 }
 
